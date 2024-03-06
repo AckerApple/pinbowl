f5bbc257748ce1bbfa3985457b093800a509c93e
Acker Apple <y>
2024-03-06 09:47:45 -0500

    update

--- taggedjs/processNewValue.function.d.ts
+++
@@ -1,2 +0,0 @@
-import { Context, Tag } from "./Tag.class.js";
-export declare function processNewValue(hasValue: boolean, value: any, context: Context, variableName: string, ownerTag: Tag): void;
--- taggedjs/renderExistingTag.function.d.ts
+++
@@ -1,5 +0,0 @@
-import { Tag } from "./Tag.class.js";
-import { TagSupport } from "./TagSupport.class.js";
-import { TemplaterResult } from "./templater.utils.js";
-/** Returns true when rendering owner is not needed. Returns false when rendering owner should occur */
-export declare function renderExistingTag(tag: Tag, newTemplater: TemplaterResult, tagSupport: TagSupport): boolean;
