159b6a0d0a5bf5c51b0ab0efc94e28dfed9acefe
Acker Apple <y>
2024-05-07 11:32:56 -0400

    update

--- src/taggedjs/js/alterProps.function.js
+++ src/taggedjs/js/alterProps.function.js
@@ -1,17 +1,17 @@
+import { deepClone, deepEqual } from './deepFunctions';
 import { isTag } from './isInstance';
 import { renderTagSupport } from './renderTagSupport.function';
+import { setUse } from './state';
+import { isInCycle } from './tagRunner';
 /* Used to rewrite props that are functions. When they are called it should cause parent rendering */
 export function alterProps(props, ownerSupport) {
-    function callback(toCall, callWith) {
-        return callbackPropOwner(toCall, callWith, ownerSupport);
-    }
     const isPropTag = isTag(props);
     const watchProps = isPropTag ? 0 : props;
-    const newProps = resetFunctionProps(watchProps, callback);
+    const newProps = resetFunctionProps(watchProps, ownerSupport);
     return newProps;
 }
-function resetFunctionProps(props, callback) {
-    if (typeof (props) !== 'object') {
+function resetFunctionProps(props, ownerSupport) {
+    if (typeof (props) !== 'object' || !ownerSupport) {
         return props;
     }
     const newProps = props;
@@ -19,15 +19,17 @@ function resetFunctionProps(props, callback) {
     // const newProps = {...props} 
     Object.entries(newProps).forEach(([name, value]) => {
         if (value instanceof Function) {
-            const original = newProps[name].original;
-            if (original) {
+            const toCall = newProps[name].toCall;
+            if (toCall) {
                 return; // already previously converted
             }
             newProps[name] = (...args) => {
                 return newProps[name].toCall(...args); // what gets called can switch over parent state changes
             };
             // Currently, call self but over parent state changes, I may need to call a newer parent tag owner
-            newProps[name].toCall = (...args) => callback(value, args);
+            newProps[name].toCall = (...args) => {
+                return callbackPropOwner(value, args, ownerSupport);
+            };
             newProps[name].original = value;
             return;
         }
@@ -35,9 +37,31 @@ function resetFunctionProps(props, callback) {
     return newProps;
 }
 export function callbackPropOwner(toCall, callWith, ownerSupport) {
-    const callbackResult = toCall(...callWith);
-    const lastestOwner = ownerSupport.global.newest;
-    renderTagSupport(lastestOwner, true);
-    return callbackResult;
+    // const renderCount = ownerSupport.global.renderCount
+    const cycle = isInCycle();
+    const result = toCall(...callWith);
+    const run = () => {
+        const lastestOwner = ownerSupport.global.newest;
+        if (cycle) {
+            // appears a prop function was called sync/immediately so lets see if owner changed state
+            const allMatched = lastestOwner.memory.state.every(state => {
+                const lastValue = state.lastValue;
+                const get = state.get();
+                const equal = deepEqual(deepClone(lastValue), get);
+                return equal;
+            });
+            if (allMatched) {
+                return result; // owner did not change
+            }
+        }
+        const newest = renderTagSupport(lastestOwner, true);
+        lastestOwner.global.newest = newest;
+        return result;
+    };
+    if (!cycle) {
+        return run();
+    }
+    setUse.memory.tagClosed$.toCallback(run);
+    return result;
 }
 //# sourceMappingURL=alterProps.function.js.map
--- taggedjs/js/interpolations/bindSubjectCallback.function.js
+++ taggedjs/js/interpolations/bindSubjectCallback.function.js
@@ -24,13 +24,15 @@ export function runTagCallback(value, tagSupport, bindTo, args) {
         }
         return 'no-data-ever'; // already rendered
     }
-    renderTagSupport(tagSupport, true);
+    const newest = renderTagSupport(tagSupport.global.newest, true);
+    tagSupport.global.newest = newest;
     if (callbackResult instanceof Promise) {
         return callbackResult.then(() => {
             if (tagSupport.global.deleted) {
                 return 'promise-no-data-ever'; // tag was deleted during event processing
             }
-            renderTagSupport(tagSupport, true);
+            const newest = renderTagSupport(tagSupport.global.newest, true);
+            tagSupport.global.newest = newest;
             return 'promise-no-data-ever';
         });
     }
